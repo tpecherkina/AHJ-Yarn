42c1411e5e273b2fb7217f3201011a51a1d009c4
tpecherkina <74115313+tpecherkina@users.noreply.github.com>
2022-05-01 17:51:06 -0400

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,4 @@
-// TODO: write code here
-
-// comment this to pass build
-//const unusedVariable = 'variable';
-
-// for demonstration purpose only
+const unusedVariable = 'variable';
 export default function demo(value) {
   return value;
 }
-
